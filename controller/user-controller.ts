2c00cce994de8f3a54f41bd455509054b7c14f20
Ilya Nikiforov <ilija8897@yandex.ru>
2023-05-06 22:43:06 +0300

    add test response

--- controller/user-controller.ts
+++ controller/user-controller.ts
@@ -1,24 +1,34 @@
-import { userService } from '../services/user-service.js';
+import { userService } from "../services/user-service.js";
 
 const userController = {
-    registration: async (
-        req: any,
-        res: { json: (arg: string[]) => any },
-        next: any
-        // eslint-disable-next-line consistent-return
-    ) => {
-        console.log(req);
-        try {
-            const { email, password } = req.body;
-            const userData: any = await userService.registration(
-                email,
-                password
-            );
-            console.log(userData);
-            return res.json(userData);
-        } catch (e) {
-            console.log(e);
-        }
-    },
+  registration: async (
+    req: any,
+    res: { json: (arg: string[]) => any },
+    next: any
+    // eslint-disable-next-line consistent-return
+  ) => {
+    console.log(req);
+    try {
+      const { email, password } = req.body;
+      const userData: any = await userService.registration(email, password);
+      console.log(userData);
+      return res.json(userData);
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  getData: async (
+    req: any,
+    res: { json: (arg: Record<string, string>) => any },
+    next: any
+    // eslint-disable-next-line consistent-return
+  ) => {
+    console.log("REQ", req);
+    try {
+      return res.json({ status: "SUCCESS" });
+    } catch (e) {
+      console.log(e);
+    }
+  },
 };
 export default userController;
--- router/index.ts
+++ router/index.ts
@@ -1,13 +1,13 @@
-import { Router } from 'express';
-import userController from '../controller/user-controller.js';
+import { Router } from "express";
+import userController from "../controller/user-controller.js";
 
 const router = Router();
 
-router.post('/registration', userController.registration);
-router.post('/login');
-router.post('/logout');
-router.post('/activate/:id');
-router.post('/refresh');
-router.post('/get-data');
+router.post("/registration", userController.registration);
+router.post("/login");
+router.post("/logout");
+router.post("/activate/:id");
+router.post("/refresh");
+router.post("/get-data", userController.getData);
 
 export default router;
